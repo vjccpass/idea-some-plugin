4d90becbeeb2f6f55efa4ca936d64c018150dffe
GitHub Action <action@github.com>
2022-02-24 11:01:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/vjccpass/ideasomeplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/vjccpass/ideasomeplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.vjccpass.ideasomeplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.vjccpass.ideasomeplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/vjccpass/ideasomeplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/vjccpass/ideasomeplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.vjccpass.ideasomeplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vjccpass.ideasomeplugin.MyBundle
 
 class MyApplicationService {
 
